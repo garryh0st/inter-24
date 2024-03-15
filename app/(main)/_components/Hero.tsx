12672c34ed182f67bc38a991ff106e008f13b6e8
ARNAVVERMAbang <arnav.verma.feb.2009@gmail.com>
2024-03-15 14:03:08 +0530

    UI : Navbar fix

--- app/(main)/_components/Hero.tsx
+++ app/(main)/_components/Hero.tsx
@@ -1,11 +1,35 @@
 import React from "react";
+import { cn } from "@/lib/utils";
+import { Montserrat } from "next/font/google";
+
+const Mont = Montserrat({
+  weight: "500",
+  subsets: ["latin"],
+});
 
 const Hero = () => {
   return (
-    <div className="bg-gradient-to-t from-[#1e1e1e] to-transparent from-[10%] to-[80%] h-screen  flex text-white items-end ">
-      <div className="text-lg absolute inset-0 -z-10 h-full w-full bg-[#1e1e1e] bg-[linear-gradient(to_right,#b83eff20_2px,transparent_2px),linear-gradient(to_bottom,#b83eff20_2px,transparent_2px)] bg-[size:30px_30px]"></div>
-      Wartex
-    </div>
+    <>
+      <div className="overflow-hidden bg-gradient-to-t from-[#1e1e1e] to-transparent from-[10%] to-[80%] h-screen  flex text-white items-end ">
+        <div className="text-lg absolute inset-0 -z-10 h-full w-full bg-[#1e1e1e] bg-[linear-gradient(to_right,#b83eff20_2px,transparent_2px),linear-gradient(to_bottom,#b83eff20_2px,transparent_2px)] bg-[size:30px_30px]"></div>
+        <div
+          className={cn(
+            "leading-[7.8vh]  w-full text-9xl lg:text-[300px] tracking-wide translate-y-4 px-4 text-[#b83eff] eger absolute"
+          )}
+        >
+          WARTEX #10.0
+        </div>{" "}
+        <div
+          className={cn(
+            "flex w-[900px] text-[#ff92fb]  text-2xl mx-4 mb-4  justify-between",
+            Mont.className
+          )}
+        >
+          <div>S.R.D.A.V Public School, Dayanand Vihar</div>
+          <div className="tracking-[2px]">XX/XX/24</div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,30 +1,210 @@
+"use client";
+
+import "@/public/styles/nav.css";
 import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import { Button } from "../ui/button";
+import { useState } from "react";
 
 export const Navbar = () => {
-  return (
-    <div className="px-8 bg-[#1e1e1e60] backdrop-blur-lg items-center fixed  text-white  flex w-screen justify-between h-[80px]  ">
-      <Image alt="logo" src={"/wartex-logo.webp"} height={60} width={60} />
+  const [navOpen, setNavOpen] = useState(false);
 
-      <div className=" gap-x-5 flex items-center  h-full">
-        <Link href={"/Legacy"} className="px-4 font-bold ">
-          Legacy
-        </Link>
-        <Link href={"/Team"} className="px-4 font-bold ">
-          Team
-        </Link>
-        <Link href={"/Events"} className="px-4 font-bold ">
-          Events
-        </Link>
-        <Link href={"/Contact"} className="px-4 font-bold ">
-          Contact
-        </Link>
-        <Link href={"/abc"}>
-          <Button className="font-bold text-md px-8 py-6">Register</Button>
-        </Link>
+  return (
+    <>
+      <div className="nav">
+        <div className="nav-container">
+          <div className="navbar">
+            <div className="logo">
+              <Image
+                src="/wartex-logo.webp"
+                alt="logo"
+                height={60}
+                width={60}
+              />
+            </div>
+            <div
+              className="menu-toggle"
+              onClick={() => {
+                setNavOpen(!navOpen);
+              }}
+            >
+              <div className={navOpen ? "hamBox hamBoxOpen" : "hamBox"}>
+                <span className={navOpen ? "lineTop spin" : "lineTop"}></span>
+                <span
+                  className={navOpen ? "lineBottom spin" : "lineBottom"}
+                ></span>
+              </div>
+            </div>
+          </div>
+          {/* <ReactLenis root wrapper duration={1.4} orientation="vertical"> */}
+          <div
+            className="nav-overlay"
+            style={{
+              top: navOpen ? "0" : "-100vh",
+              transitionDelay: navOpen ? "0s" : "0s",
+            }}
+          >
+            <div>
+              <div
+                className="menu-toggle menu-close-btn flex justify-end  translate-x-10 -translate-y-10"
+                onClick={() => {
+                  setNavOpen(!navOpen);
+                }}
+              >
+                <div className={navOpen ? "hamBox hamBoxOpen" : "hamBox"}>
+                  <span className={navOpen ? "lineTop spin" : "lineTop"}></span>
+                  <span
+                    className={navOpen ? "lineBottom spin" : "lineBottom"}
+                  ></span>
+                </div>
+              </div>
+            </div>
+            <ul className="nav-links">
+              <li className="nav-item">
+                <a
+                  href="#"
+                  onClick={() => setNavOpen(!navOpen)}
+                  style={{
+                    top: navOpen ? "0px" : "120px",
+                    color: navOpen ? "white" : "#00000000",
+                    opacity: navOpen ? "1" : "0",
+                    transitionDelay: navOpen ? "1s" : "0s",
+                  }}
+                >
+                  Legacy
+                </a>
+                <div className="nav-item-wrapper"></div>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#"
+                  onClick={() => setNavOpen(!navOpen)}
+                  style={{
+                    top: navOpen ? "0px" : "120px",
+                    color: navOpen ? "white" : "#00000000",
+                    opacity: navOpen ? "1" : "0",
+                    transitionDelay: navOpen ? "0.8s" : "0s",
+                  }}
+                >
+                  TEAM
+                </a>
+                <div className="nav-item-wrapper"></div>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#"
+                  onClick={() => setNavOpen(!navOpen)}
+                  style={{
+                    top: navOpen ? "0px" : "120px",
+                    color: navOpen ? "white" : "#00000000",
+                    opacity: navOpen ? "1" : "0",
+                    transitionDelay: navOpen ? "0.6s" : "0s",
+                  }}
+                >
+                  ARTICLES
+                </a>
+                <div className="nav-item-wrapper"></div>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#"
+                  onClick={() => setNavOpen(!navOpen)}
+                  style={{
+                    top: navOpen ? "0px" : "120px",
+                    color: navOpen ? "white" : "#00000000",
+                    opacity: navOpen ? "1" : "0",
+                    transitionDelay: navOpen ? "0.4s" : "0s",
+                  }}
+                >
+                  Contact
+                </a>
+                <div className="nav-item-wrapper"></div>
+              </li>
+            </ul>
+            <div className="nav-footer">
+              {/* <div
+                className="location mr:0 md:mr-8"
+                style={{
+                  bottom: navOpen ? "0px" : "120px",
+                  color: navOpen ? "white" : "#00000000",
+                  opacity: navOpen ? "1" : "0",
+                  transitionDelay: navOpen ? "0.8s" : "0s",
+                  fontSize: "1.3vw",
+                }}
+              >
+                <span className="sf">Delhi, IN</span>
+              </div> */}
+              <div className="nav-social-media hidden sm:block">
+                <ul>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/arnav.tsx/"
+                      className="sf mr:0 md:mr-8 socialLinks hidden sm:block"
+                      style={{
+                        bottom: navOpen ? "0px" : "120px",
+                        color: navOpen ? "white" : "#00000000",
+                        opacity: navOpen ? "1" : "0",
+                        transitionDelay: navOpen ? "0.8s" : "0s",
+                        fontSize: "1.3vw",
+                      }}
+                    >
+                      Instagram
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="sf socialLinks hidden sm:block"
+                      href="https://github.com/ArnavVerma-2009"
+                      style={{
+                        bottom: navOpen ? "0px" : "120px",
+                        color: navOpen ? "white" : "#00000000",
+                        opacity: navOpen ? "1" : "0",
+                        transitionDelay: navOpen ? "0.8s" : "0s",
+                        fontSize: "1.3vw",
+                      }}
+                    >
+                      Github
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="nav-social-media block sm:hidden">
+                <ul>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/arnav.tsx/"
+                      className="mr:0 md:mr-8 socialLinks "
+                      style={{
+                        bottom: navOpen ? "0px" : "120px",
+                        color: navOpen ? "white" : "#00000000",
+                        opacity: navOpen ? "1" : "0",
+                        transitionDelay: navOpen ? "0.8s" : "0s",
+                        fontSize: "5vw",
+                      }}
+                    >
+                      Instagram
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="socialLinks"
+                      href="https://github.com/ArnavVerma-2009"
+                      style={{
+                        bottom: navOpen ? "0px" : "120px",
+                        color: navOpen ? "white" : "#00000000",
+                        opacity: navOpen ? "1" : "0",
+                        transitionDelay: navOpen ? "0.8s" : "0s",
+                        fontSize: "5vw",
+                      }}
+                    >
+                      Github
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* </ReactLenis> */}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
